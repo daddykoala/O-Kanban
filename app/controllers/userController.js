91e59f7437e5975c0067808862637dc502d817c4
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-05-20 14:49:25 +0200

    add data query login

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,10 +1,12 @@
 const errorHandling = require("../middlewares/errorHandling");
-const { User, Table } = require("../models");
+const { User, Table, List, Card, Tag } = require("../models");
 const crypto = require("crypto");
-const { generateAccessToken, generateRefreshToken } = require("../service/jsonWebToken");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../service/jsonWebToken");
 
 const userController = {
-
   async getAllUsers(req, res) {
     try {
       const users = await User.findAll({ include: "table" });
@@ -17,21 +19,47 @@ const userController = {
     try {
       const { email } = req.body;
 
-      const foundUser = await User.findOne({ where: { email },include:"table" });
-      if(foundUser){
+      const foundUser = await User.findOne({
+        where: { email },
+        include: [
+          {
+            model: Table,
+            as: "table",
+            include: [
+              {
+                model: List,
+                as: "list",
+                include: [
+                  {
+                    model: Card,
+                    as: "card",
+                    include: [
+                      {
+                        model: Tag,
+                        as: "tag",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      });
 
-//*création du JWT
+      if (foundUser) {
+        //*création du JWT
         const accessToken = generateAccessToken(foundUser.email);
-//* création du refreshToken
-      const refreshToken = generateRefreshToken(foundUser.email);
-      console.log("canevoi du cookies");
-      res.cookie("jwt", refreshToken,
-			{httpOnly: true,
-				// sameSite: 'None',
-				// secure: true, 
-				maxAge: 24 * 60 * 60 * 1000
-			 });
-        res.status(200).json({user:foundUser,token:accessToken});
+        //* création du refreshToken
+        const refreshToken = generateRefreshToken(foundUser.email);
+        console.log("canevoi du cookies");
+        res.cookie("jwt", refreshToken, {
+          httpOnly: true,
+          // sameSite: 'None',
+          // secure: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        });
+        res.status(200).json({ user: foundUser, token: accessToken });
       }
       if (!foundUser) {
         res.status(404).json({ message: `User not found with email ${email}` });
@@ -45,41 +73,43 @@ const userController = {
     // console.log(User);
     try {
       const userId = parseInt(req.params.id);
-      console.log( typeof userId );
+      console.log(typeof userId);
       console.log(req.params.id);
       console.log(userId);
 
-        const user = await User.findByPk(userId, {
-            include:[{
-              association:"table",
-              include:[{
-                association:"list",
-                include:[{
-                  association:"card",
-                  include:[{association:"tag"}]}
-                ]}]
-          }],
-           
-        });
-        if (!user) {
-          
-          res.status(404).json({message:`Cant find user with id ${userId}`});
+      const user = await User.findByPk(userId, {
+        include: [
+          {
+            association: "table",
+            include: [
+              {
+                association: "list",
+                include: [
+                  {
+                    association: "card",
+                    include: [{ association: "tag" }],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      });
+      if (!user) {
+        res.status(404).json({ message: `Cant find user with id ${userId}` });
+      } else {
+        // console.log("je renvoi bien le bouzin",user.dataValues.id);
 
-        } else {
-          // console.log("je renvoi bien le bouzin",user.dataValues.id);
-          
-            res.json(user);
-        }
+        res.json(user);
+      }
     } catch (error) {
-        errorHandling.log(error);
+      errorHandling.log(error);
     }
-    },
+  },
 
   async createUser(req, res) {
-    
     try {
-      const {email ,name,lastname} = req.body;
-       
+      const { email, name, lastname } = req.body;
 
       let bodyErrors = [];
       if (!name) {
@@ -88,16 +118,16 @@ const userController = {
       if (!lastname) {
         bodyErrors.push(`name can not be empty`);
       }
-      
-      if (!email ) {
+
+      if (!email) {
         bodyErrors.push(`Invalid email`);
       }
 
       // if (bodyErrors.length) {
       //   res.status(400).json(bodyErrors);
-      // } 
+      // }
       else {
-        let newUser = User.build({ name,lastname, email });
+        let newUser = User.build({ name, lastname, email });
         await newUser.save();
         res.json(newUser);
       }
@@ -141,7 +171,6 @@ const userController = {
         await user.destroy();
         res.json("ok");
       }
-      
     } catch (error) {
       console.trace(error);
       res.status(500).json(error);
